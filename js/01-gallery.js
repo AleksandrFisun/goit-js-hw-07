7f33396b6b14ca43b1918bbba80d2919a66dc532
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2022-07-29 01:13:33 +0300

    Дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const containerGallaryDivRef = document.querySelector(".gallery");
+const marcupRef = createGellery(galleryItems);
+containerGallaryDivRef.insertAdjacentHTML("beforeend", marcupRef);
+
+function createGellery(event) {
+  return event
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            title="${description}"
+            loading="lazy"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+containerGallaryDivRef.addEventListener("click", (event) => {
+  event.preventDefault();
+  const srcImgEl = event.target.dataset.source;
+  const addBasicLightboxImg = basicLightbox.create(`
+    <img src="${srcImgEl}" width="800" height="600">
+`);
+
+  addBasicLightboxImg.show();
+});
+containerGallaryDivRef.addEventListener("keydown", (event) => {
+  if (event.code === "Escape") {
+    document.querySelector("div.basicLightbox").remove("");
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const containerGallaryDivRef = document.querySelector(".gallery");
+const marcupRef = createGellery(galleryItems);
+containerGallaryDivRef.insertAdjacentHTML("beforeend", marcupRef);
+
+function createGellery(event) {
+  return event
+    .map(({ preview, original, description }) => {
+      return `<a 
+      class="gallery__item"
+      href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}" 
+  alt="${description}" />
+</a>
+`;
+    })
+    .join("");
+}
+
+containerGallaryDivRef.addEventListener("click", modalImg);
+
+var modalImg = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captions: true,
+  captionType: "attr",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
